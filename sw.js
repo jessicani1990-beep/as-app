82c26810c43989c698a1c045932b6ac23c306a48
jessicani1990-beep <jessicani1990@gmail.com>
2026-04-30 00:23:31 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,17 @@
-const CACHE_NAME = 'as-pro-v4';
-const ASSETS = ['./index.html', './manifest.json'];
+const CACHE_NAME = 'as-pro-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json'
+];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
