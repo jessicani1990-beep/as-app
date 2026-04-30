dd7b63db8dfdd837d5b6f74dbb0eb0b5d5c94d3f
jessicani1990-beep <jessicani1990@gmail.com>
2026-04-30 22:08:55 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,37 @@
-const CACHE_NAME = 'as-ppp';
-const ASSETS = ['./index.html', './manifest.json'];
+const CACHE_NAME = 'as-app'; 
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'icon.png' 
+];
+
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('正在快取資源');
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting(); 
 });
 
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
 });
